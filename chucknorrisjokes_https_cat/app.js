f6ebb93edd50f575310a30e1119de69d4ccf8cde
Carl Storms <37883693+TheBIMsider@users.noreply.github.com>
2022-01-31 14:14:25 -0700

    CMJG with Categories

---
+++ chucknorrisjokes_https_cat/app.js
@@ -0,0 +1,41 @@
+document.querySelector(".random-joke").addEventListener("click", getRanJoke);
+
+function getRanJoke(e) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random", true);
+
+  xhr.onload = function() {
+
+      if(this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        document.querySelector(".jokes").innerHTML = `<h1>${response.value}</h1>`;
+      }
+    }
+
+  xhr.send();
+
+  e.preventDefault();
+}
+
+document.querySelector(".category-joke").addEventListener("click", getCatJoke);
+
+function getCatJoke(e) {
+  let category = document.querySelector("select").value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `https://api.chucknorris.io/jokes/random?category=${category}`, true);
+console.log(category);
+  xhr.onload = function() {
+
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      document.querySelector(".jokes").innerHTML = `<h2>${response.value}</h2>`;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
